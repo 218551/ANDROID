d95704c1c9d38f2f3c489a6c08e46eb1b02cccef
tomas <tomaszxkubat@gmail.com>
2019-04-20 19:58:19 +0200

    20.04.2019

---
+++ ProjektZespolowy/app/src/main/java/com/example/barcode_beta/Constants.java
@@ -0,0 +1,8 @@
+package com.example.barcode_beta;
+
+public class Constants {
+    public static final String ROOT_URL = "http://192.168.0.3/projectconnection/";
+    public static final String URL_SHOW = ROOT_URL + "dbProductSearch.php";
+}
+
+
